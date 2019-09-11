00336f3d0bb942bb0ddadef0c03a08f84e8ae71f
lisperow <lisperow@yandex.ru>
2019-09-11 17:37:54 +0300

    add methods

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -37,6 +37,26 @@ export default {
       const { baseUrl, part, type, order, maxResults, q, key } = this.api;
       const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}`;
       this.getData(apiUrl);
+    },
+    prevPage() {
+      const { baseUrl, part, type, order, maxResults, q, key, prevPageToken } = this.api;
+      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${prevPageToken}`;
+      this.getData(apiUrl);
+    },
+    nextPage() {
+      const { baseUrl, part, type, order, maxResults, q, key, nextPageToken } = this.api;
+      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${nextPageToken}`;
+      this.getData(apiUrl);
+    },
+    getData(apiUrl) {
+      axios
+        .get(apiUrl)
+        .then(res => {
+          this.videos = res.data.items;
+          this.api.prevPageToken = res.data.prevPageToken;
+          this.api.nextPageToken = res.data.nextPageToken;
+        })
+        .catch(e => console.log(e));
     }
   }
 }
